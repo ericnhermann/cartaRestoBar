b354804775ee092754b0719e4992992f5250bd2a
FDC248150 <fc_9902@outlook.com>
2023-10-04 16:56:56 -0300

    intenteJS

---
+++ cartaRestoBar/scriptCartaResto.js
@@ -0,0 +1,5 @@
+// JavaScript source code
+function displayEnvio() {
+    
+    document.getElementById("demo").innerHTML = "Se ha enviado su solicitud correctamente, le enviaremos una confirmación de su reserva mediante e-mail.";
+}
