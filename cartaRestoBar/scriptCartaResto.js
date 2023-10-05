be929df5a3c9d301a0929058bdd89ae74bd562bb
FDC248150 <fc_9902@outlook.com>
2023-10-05 19:54:15 -0300

    borrando y descomentando

--- cartaRestoBar/scriptCartaResto.js
+++ cartaRestoBar/scriptCartaResto.js
@@ -3,19 +3,7 @@ function displayEnvio() {
     
     document.getElementById("demo").innerHTML = "Se ha enviado su solicitud correctamente, le enviaremos una confirmación de su reserva mediante e-mail.";
 }
-(function(){
-    (document).on('keyup','#foo',function(){
-      var val = $(this).val().trim(),
-          reg = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-      if( reg.test(val) == false ){
-        console.log('NO es un mail');
-      }
-      
-      else{
-        console.log('SI es un mail');
-      }
-    });
-  });
+
 
 function mostrarInfoConCheckeo(){
 
