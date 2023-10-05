a94b9ea482c5e7ef1a2929629be86bb73c32906c
francoo113 <franco-githubemail@proton.me>
2023-10-05 19:50:29 -0300

    displayInfo_v2

--- cartaRestoBar/scriptCartaResto.js
+++ cartaRestoBar/scriptCartaResto.js
@@ -16,3 +16,15 @@ function displayEnvio() {
       }
     });
   });
+
+function mostrarInfoConCheckeo(){
+
+    var email = document.getElementById("mailreserva").value;
+    var nombreReserva = document.getElementById("nombrereserva").value;
+
+    if(email == "" || nombreReserva == ""){
+        document.getElementById("demo").innerHTML = "El formulario debe completarse entero";
+    } else {
+        document.getElementById("demo").innerHTML = "Se ha enviado su solicitud correctamente, le enviaremos una confirmación de su reserva mediante e-mail.";
+    }
+}
