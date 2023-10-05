4c28021bbe694f2fec54db8b079f0d15e99bb2b6
FDC248150 <fc_9902@outlook.com>
2023-10-05 20:39:27 -0300

    p

--- cartaRestoBar/scriptCartaResto.js
+++ cartaRestoBar/scriptCartaResto.js
@@ -5,13 +5,15 @@ function displayEnvio() {
 }
 
 
-function mostrarInfoConCheckeo(){
-
+function mostrarInfoConCheckeo() {
     var email = document.getElementById("mailreserva").value;
     var nombreReserva = document.getElementById("nombrereserva").value;
 
-    if(email == "" || nombreReserva == ""){
-        document.getElementById("demo").innerHTML = "El formulario debe completarse entero";
+    // Expresión regular para validar el formato del correo electrónico
+    var regex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+
+    if(email == "" || nombreReserva == "" || !regex.test(email)) {
+        document.getElementById("demo").innerHTML = "El formulario debe completarse entero y el correo electrónico debe ser válido";
     } else {
         document.getElementById("demo").innerHTML = "Se ha enviado su solicitud correctamente, le enviaremos una confirmación de su reserva mediante e-mail.";
     }
